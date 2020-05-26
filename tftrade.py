1543ec85ebba004a772c2ad3e99f24faebfc2dd3
cheffplayer <dakrav51@gmail.com>
2020-05-26 02:00:29 -0700

    basic api implementation

--- tftrade.py
+++ tftrade.py
@@ -6,11 +6,7 @@ import time
 webdriver = r"C:\Users\assan\AppData\Local\Programs\Python\chromedriver.exe"
 
 driver = Chrome(webdriver)
-
-
-url = "https://tftrade.net" 
-
-driver.get(url)
+driver.get("https://tftrade.net" )
 
 time.sleep (25) #wait for site to load and put eventual search filter
 
@@ -19,12 +15,35 @@ items = driver.find_elements_by_class_name("item")
 for item in items :
     name = item.find_element_by_class_name("name").get_attribute('innerText')
     price = item.find_element_by_id("price")
-    print (name, price.text)
 
+    #api url (only works for unique items!)
+    url = ('https://backpack.tf/api/classifieds/search/v1?item_names=1&item=' + str(name) + '&quality=6&tradable=1&craftable=1&page_size=30&key=apikey')
 
+    #takes the json data from the api url and converts it to a dictionary
+    dict = (requests.get(url)).json()
 
+    #this tries to find unpainted/unfestivized items
+    for x in range(len(dict['buy']['listings'])):
+        try:
+            if dict['buy']['listings'][x]['item']['attributes']:
+                ()
+        except KeyError:
+            break
 
+    #this tries to find out if the item has a key or metal value. if one of them is missing, then the value gets set to 0
+    for bpmetalX in dict['buy']['listings'][x]['currencies']:
+        try:
+            if dict['buy']['listings'][x]['currencies']['metal']:
+                bpmetal = dict['buy']['listings'][x]['currencies']['metal']
+        except KeyError:
+            bpmetal = 0
 
+    for bpkeysX in dict['buy']['listings'][x]['currencies']:
+        try:
+            if dict['buy']['listings'][x]['currencies']['keys']:
+                bpkeys = dict['buy']['listings'][x]['currencies']['keys']
+        except KeyError:
+            bpkeys = 0
 
-
-
+    print(name, price.text)
+    print('backpack price: ', 'keys: ', bpkeys, 'metal: ', bpmetal)
